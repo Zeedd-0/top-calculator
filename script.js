8a90cd4a56f0c896947eab9cd270dfb6351a58eb
Zeedd-0 <110570899+Zeedd-0@users.noreply.github.com>
2024-10-14 21:58:53 -0300

    Create functions, eventlisteners

--- script.js
+++ script.js
@@ -1,31 +1,119 @@
 // ==================================
 function add(a, b) {
-    return a + b;
+    return parseFloat(a) + parseFloat(b);
 }
 
 function subtract(a, b) {
-    return a - b;
+    return parseFloat(a) - parseFloat(b);
 }
 
 function multiply(a, b) {
-    return a * b;
+    return parseFloat(a) * parseFloat(b);
 }
 
 function divide(a, b) {
-    return a / b;
+    return parseFloat(a) / parseFloat(b);
 }
 
-// ==================================
+// ----------------------------------
 function operate(fNum, sNum, operation) {
-    return operator[operation](fNum, sNum);
+    return OPERATOR[operation](fNum, sNum).toFixed(3) * 1;
 }
 
 // ==================================
-let fNum = 0;
-let sNum = 0;
-const operator = {
-    add: add,
-    sub: subtract,
-    mult: multiply,
-    div: divide,
+const RESULT_DISPLAY = document.querySelector('#result-display');
+const OPERATION_DISPLAY = document.querySelector('#operation-display');
+const BTN_NUMBER = document.querySelectorAll('.button.number');
+const BTN_OPERATION = document.querySelectorAll('.button.operation')
+const BTN_END = document.querySelectorAll('#equals, #clear');
+
+const OPERATOR = {
+    'add': add,
+    'subtract': subtract,
+    'multiply': multiply,
+    'divide': divide,
+};
+
+const NUMBER = {
+    first: '',
+    second: '',
+    result: '',
+    selOperation: '',
+}
+
+// ==================================
+BTN_NUMBER.forEach(button => {
+    button.addEventListener('click', e => {
+        makeNum(e.target);
+    });
+})
+
+BTN_OPERATION.forEach(button => {
+    button.addEventListener('click', e => {
+        let  target = e.target;
+        if (target.id !== 'equals') {
+            if (NUMBER.result !== '' && NUMBER.first === '') {
+                NUMBER.first = NUMBER.result;
+                NUMBER.result = '';
+            };
+            NUMBER.selOperation = target;
+            display(target.innerText, 0);
+        };
+    });
+})
+
+BTN_END.forEach(button => {
+    button.addEventListener('click', e => {
+        switch (e.target.id) {
+            case 'clear':
+                clear();
+                break;
+            case 'equals':
+                equals();
+                break;
+        };
+    });
+})
+
+// ----------------------------------
+
+function makeNum(target) {
+    let overwrite = 0;
+    if (NUMBER.selOperation) {
+        NUMBER.second += target.innerText;
+    } else {
+        if (NUMBER.result) {
+            overwrite = 1;
+            NUMBER.result = '';
+        };
+        NUMBER.first += target.innerText;
     };
+    display(target.innerText, overwrite);
+}
+
+function display(text, overwrite) {
+    if (overwrite) {
+        RESULT_DISPLAY.innerText = text;    
+    } else {
+        RESULT_DISPLAY.innerText += text;
+    };
+}
+
+function equals() {
+    if (NUMBER.selOperation) {
+        let result = operate(NUMBER.first, NUMBER.second, NUMBER.selOperation.id);
+        NUMBER.first = '';
+        NUMBER.second = '';
+        NUMBER.result = result;
+        NUMBER.selOperation = '';
+        display(result, 1);
+    };
+}
+
+function clear() {
+    NUMBER.first = '';
+    NUMBER.second = '';
+    NUMBER.result = '';
+    NUMBER.selOperation = '';
+    display('', 1);
+}
